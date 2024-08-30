3db72588cd668c0646e1aeb8e5c4f336bedf4368
이종혁 <whdgur57117@gmail.com>
2024-08-31 01:54:16 +0900

    fix bug

--- src/base.js
+++ src/base.js
@@ -1,9 +1,11 @@
 module.exports = {
   extends: ["plugin:import/errors", "eslint:recommended"],
   plugins: ["json-format", "prettier", "simple-import-sort"],
-  "simple-import-sort/imports": "error",
-  "simple-import-sort/exports": "error",
-  "import/no-unresolved": "off",
-  "import/prefer-default-export": "off",
-  "import/extensions": "off",
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/no-unresolved": "off",
+    "import/prefer-default-export": "off",
+    "import/extensions": "off",
+  },
 }
